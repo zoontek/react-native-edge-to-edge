c5783a2f389c739122a80d0b06b2ffb894bbe472
Mathieu Acthernoene <zoontek@gmail.com>
2023-04-08 22:26:17 +0200

    Fix missing init

--- example/android/app/src/main/java/com/rnbarsexample/MainActivity.java
+++ example/android/app/src/main/java/com/rnbarsexample/MainActivity.java
@@ -1,9 +1,12 @@
 package com.rnbarsexample;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import com.zoontek.rnbars.RNBars;
 
 public class MainActivity extends ReactActivity {
 
@@ -14,6 +17,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "RNBarsExample";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+    RNBars.init(this, "dark-content");
   }
 
   /**
