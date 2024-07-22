d1a3fd597f9470e0301658f2914d5ff8ddf58b57
Mathieu Acthernoene <zoontek@gmail.com>
2024-07-22 20:49:07 +0200

    Use SegmentedControls

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,15 +1,15 @@
+import SegmentedControl from "@react-native-segmented-control/segmented-control";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import * as React from "react";
+import { useState } from "react";
 import {
   Appearance,
   StyleSheet,
-  Switch,
   Text,
   useColorScheme,
   View,
 } from "react-native";
-import { NavigationBar, StatusBar } from "react-native-bars";
+import { NavigationBar, StatusBar, SystemBarStyle } from "react-native-bars";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const styles = StyleSheet.create({
@@ -19,29 +19,15 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     paddingHorizontal: 16,
   },
+  darkContainer: {
+    backgroundColor: "#1F2937",
+  },
   title: {
     color: "#374151",
     fontSize: 20,
     fontWeight: "700",
   },
-  row: {
-    flexDirection: "row",
-  },
-  tag: {
-    backgroundColor: "#E5E7EB",
-    borderRadius: 4,
-    paddingHorizontal: 8,
-  },
-  tagText: {
-    color: "#374151",
-    fontSize: 16,
-    lineHeight: 32,
-    textAlignVertical: "center",
-  },
-  darkTag: {
-    backgroundColor: "#374151",
-  },
-  darkTagText: {
+  darkTitle: {
     color: "#E5E7EB",
   },
 });
@@ -54,98 +40,72 @@ const Space = ({ size }: { size: number }) => (
   />
 );
 
-const Line = ({
-  title,
-  left,
-  right,
-  value,
-  onValueChange,
-  dark,
-}: {
-  title: string;
-  left: string;
-  right: string;
-  value: boolean;
-  onValueChange: (value: boolean) => void;
-  dark: boolean;
-}) => (
-  <View>
-    <Text style={[styles.title, dark && { color: "#E5E7EB" }]}>{title}</Text>
-    <Space size={16} />
-
-    <View style={styles.row}>
-      <View style={[styles.tag, dark && styles.darkTag]}>
-        <Text style={[styles.tagText, dark && styles.darkTagText]}>{left}</Text>
-      </View>
-
-      <Space size={16} />
-
-      <Switch
-        onValueChange={onValueChange}
-        value={value}
-        thumbColor="#E5E7EB"
-        trackColor={{
-          true: "#9CA3AF",
-          false: "#9CA3AF",
-        }}
-      />
-
-      <Space size={16} />
-
-      <View style={[styles.tag, dark && styles.darkTag]}>
-        <Text style={[styles.tagText, dark && styles.darkTagText]}>
-          {right}
-        </Text>
-      </View>
-    </View>
-  </View>
-);
-
-const App = () => {
-  const colorScheme = useColorScheme();
-  const dark = colorScheme === "dark";
-
-  const [isLightStatus, setIsLightStatus] = React.useState(false);
-  const [isLightNavigation, setIsLightNavigation] = React.useState(false);
+const Title = ({ text }: { text: string }) => {
+  const dark = useColorScheme() === "dark";
 
   return (
-    <SafeAreaView
-      style={[styles.container, dark && { backgroundColor: "#1F2937" }]}
-    >
-      <StatusBar style={isLightStatus ? "light" : "dark"} animated={true} />
-      <NavigationBar style={isLightNavigation ? "light" : "dark"} />
+    <>
+      <Text style={[styles.title, dark && styles.darkTitle]}>{text}</Text>
+      <Space size={16} />
+    </>
+  );
+};
 
-      <Line
-        title="Theme"
-        left="dark"
-        right="light"
-        value={!dark}
-        dark={dark}
-        onValueChange={(light) => {
-          Appearance.setColorScheme(light ? "light" : "dark");
+const BAR_STYLES: Record<number, SystemBarStyle> = {
+  0: "auto",
+  1: "light",
+  2: "dark",
+};
+
+const App = () => {
+  const dark = useColorScheme() === "dark";
+
+  const [statusBarStyleIndex, setStatusBarStyleIndex] = useState(0);
+  const [navigationBarStyleIndex, setNavigationBarStyleIndex] = useState(0);
+
+  return (
+    <SafeAreaView style={[styles.container, dark && styles.darkContainer]}>
+      <StatusBar
+        style={BAR_STYLES[statusBarStyleIndex] ?? "auto"}
+        animated={true}
+      />
+
+      <NavigationBar style={BAR_STYLES[navigationBarStyleIndex] ?? "auto"} />
+
+      <Title text="Theme" />
+
+      <SegmentedControl
+        values={["light", "dark"]}
+        selectedIndex={dark ? 1 : 0}
+        onChange={({ nativeEvent: { selectedSegmentIndex } }) => {
+          Appearance.setColorScheme(
+            selectedSegmentIndex === 1 ? "dark" : "light",
+          );
         }}
       />
 
       <Space size={48} />
 
-      <Line
-        title="<StatusBar />"
-        left="dark"
-        right="light"
-        value={isLightStatus}
-        dark={dark}
-        onValueChange={setIsLightStatus}
+      <Title text="<StatusBar />" />
+
+      <SegmentedControl
+        values={Object.values(BAR_STYLES)}
+        selectedIndex={statusBarStyleIndex}
+        onChange={({ nativeEvent: { selectedSegmentIndex } }) => {
+          setStatusBarStyleIndex(selectedSegmentIndex);
+        }}
       />
 
       <Space size={32} />
 
-      <Line
-        title="<NavigationBar />"
-        left="dark"
-        right="light"
-        value={isLightNavigation}
-        onValueChange={setIsLightNavigation}
-        dark={dark}
+      <Title text="<NavigationBar />" />
+
+      <SegmentedControl
+        values={Object.values(BAR_STYLES)}
+        selectedIndex={navigationBarStyleIndex}
+        onChange={({ nativeEvent: { selectedSegmentIndex } }) => {
+          setNavigationBarStyleIndex(selectedSegmentIndex);
+        }}
       />
     </SafeAreaView>
   );
