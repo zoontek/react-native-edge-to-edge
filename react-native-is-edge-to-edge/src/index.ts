97e3e789ed7e41055f9fbdeed24642bfc78c0d59
Mathieu Acthernoene <zoontek@gmail.com>
2024-09-05 12:29:52 +0200

    Update comment

--- react-native-is-edge-to-edge/src/index.ts
+++ react-native-is-edge-to-edge/src/index.ts
@@ -1,6 +1,6 @@
 import { Platform, TurboModuleRegistry } from "react-native";
 
-// Edge-to-edge is enabled by default on all platforms, expected android
+// Edge-to-edge is only an Android problem
 const enabled =
   Platform.OS !== "android" || TurboModuleRegistry.get("RNEdgeToEdge") != null;
 
