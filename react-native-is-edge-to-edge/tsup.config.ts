343142376859de15cd981162f523fd58ca10f4de
Mathieu Acthernoene <zoontek@gmail.com>
2024-09-13 15:37:51 +0200

    Emit TS generated type definitions

--- react-native-is-edge-to-edge/tsup.config.ts
+++ react-native-is-edge-to-edge/tsup.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: { index: "src/index.ts" },
   format: ["cjs", "esm"],
-  target: "es2017",
+  target: "es2019",
   clean: true,
-  dts: true,
+  dts: false,
   sourcemap: true,
   treeshake: true,
 });
